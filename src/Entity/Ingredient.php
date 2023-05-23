bcee40e1b1db36ec8474901eb1896eaea92e11d1
ilyesLHMR <ilyes.lahmar.dev@gmail.com>
2023-05-23 22:43:41 +0200

    creation de l'entité ingredient + fixtures

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -3,24 +3,47 @@
 namespace App\Entity;
 
 use App\Repository\IngredientRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+/**
+ * @ORM\Entity(repositoryClass=IngredientRepository::class)
+ */
 class Ingredient
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
 
-    #[ORM\Column(length: 50)]
-    private ?string $name = null;
+    /**
+     * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2, max=50)
+     */
+    private string $name;
 
-    #[ORM\Column]
-    private ?float $price = null;
+    /**
+     * @ORM\Column(type="float")
+     * @Assert\NotNull()
+     * @Assert\Positive(message="This value should be positive.")
+     * @Assert\LessThan(200)
+     */
+    private float $price;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     * @Assert\NotNull()
+     */
+    private ?DateTimeImmutable $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
